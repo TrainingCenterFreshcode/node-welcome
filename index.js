2fb2629955af179782556720f20d4997bfdea102
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-27 19:13:55 +0200

    writeFile

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 
 const fs = require('fs').promises;
 
-const p = fs.readFile('./text.txt', 'utf8');
-p.then(data => console.log(data));
+fs.readFile('./text.txt', 'utf8')
+.then(content => {
+    const newContent = `1234 OLD content: ${content} and NEW content: 'Hello world'`;
+    fs.writeFile('./newfile.txt', newContent, 'utf8'); // Перезаписує файли!!!
+});
